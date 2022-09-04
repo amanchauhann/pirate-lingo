70fe06c5f24450b2be29c762e5a2a54862fd53e6
Aman Chauhan <amangotpro@gmail.com>
2022-09-04 16:30:06 +0530

    fix: API issue

---
+++ index.js
@@ -0,0 +1,55 @@
+// Input output and btn variables
+
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+var textOutput = document.querySelector("#text-output");
+
+
+
+//server url
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+// real server url
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var random;
+
+
+
+
+// error handler function
+function errorHandler(error) {
+    console.log("Ran into Error", error);
+    alert("Sorry! The server is having some issues. We are looking into it.");
+}
+
+
+// this is the url generated for translation 
+
+// function urlGenerator(text){
+
+//     return serverURL + "?text=" + text;
+// }
+
+function urlGenerator(text){
+    random = serverURL + "?text=" + text;
+       return (random)
+   }
+
+// click handler function which will be called when click happens on translate button
+
+function clickEventHandler() {
+
+    var text = textInput.value;
+
+ // processing by server 
+
+    fetch(urlGenerator(text))
+    .then(response =>response.json())
+    .then(json => textOutput.innerText = json.contents.translated ) // ouput
+    .catch(errorHandler)
+}
+
+
+btnTranslate.addEventListener('click', clickEventHandler)
